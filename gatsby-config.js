ebd75332cdd1d91198d6af9d66aaf73f21f6e473
Jorge Ramos <jorgeramos@gmail.com>
2020-07-14 19:23:08 -0400

    temporarily disable graphql and menu items

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,14 +37,14 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-source-graphql`,
-      options: {
-        typeName: `WPGraphQL`,
-        fieldName: `wpgraphql`,
-        url: `${process.env.WPGRAPHQL_URL}`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-graphql`,
+    //   options: {
+    //     typeName: `WPGraphQL`,
+    //     fieldName: `wpgraphql`,
+    //     url: `${process.env.WPGRAPHQL_URL}`,
+    //   },
+    // },
     {
       resolve: "gatsby-plugin-chakra-ui",
       options: {
