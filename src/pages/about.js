0c3d3f6c52bb3f27ac83e52fa90d8635f8dd2e37
Stephanie Pena rivera <stephanie.pena2@upr.edu>
2020-07-14 22:03:10 -0400

    wip

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,79 @@
 import React from "react";
 import DefaultLayout from "../components/layouts/DefaultLayout";
 import SEO from "../components/seo";
-import { Heading } from "@chakra-ui/core";
+import { Heading, Text, Flex, List, ListItem, Divider, Link, Image, Box } from "@chakra-ui/core";
 
 const About = () => (
   <DefaultLayout>
     <SEO title="About" />
-    <Heading as="h1">About</Heading>
+    <Heading as="h1" fontSize="50px" pb="10px">Working Together to Stop COVID-19</Heading>
+    <Text pb="60px" fontSize="25px" width={{ base:"100%", lg:"75%" }}>PathCheck Foundation is unleashing the power of a global community of organizations, researchers, health officials, and volunteers. We’re working together to build open source software, standards, and public health programs that help contain the pandemic, restart the economy, and protect individual freedom and privacy.</Text>
+    
+    <Divider width="100vw" ml="calc(50% - 50vw)"/>
+
+    <Flex py="20px">
+      <Text width={{ base:"100%", lg:"20%" }} fontSize="25px">
+        Origin
+      </Text>
+      <Text width={{ base:"100%", lg:"80%" }} fontSize="17px">
+      PathCheck was founded in February, 2020, at MIT by a passionate community of people and organizations to contain the spread of COVID-19 and help revitalize economies without sacrificing personal privacy. We’ve been pioneers in digital contact tracing and exposure notification technology. Our original technology was based on the MIT Private Kit: Safe Paths app created by Ramesh Raskar with Sandy Pentland, Kent Larson, and Kevin Esvelt.
+      </Text>
+    </Flex>
+
+    <Flex py="20px"> 
+      <Text width={{ base:"100%", lg:"20%" }} fontSize="25px">
+        Team
+      </Text>
+      <Text width={{ base:"100%", lg:"80%" }} fontSize="17px">
+      More than 1,600 volunteers around the world have joined the PathCheck community, many of them work with PathCheck full time to help stop the pandemic. More than 100 developers in the open source community contribute to our projects. In addition to volunteers, we have a core team of full-time software developers, and a wide range of companies donate employee time to our work. We hope you’ll join us.
+      </Text>
+    </Flex>
+
+    <Flex py="20px">
+      <Text width={{ base:"100%", lg:"20%" }} fontSize="25px">
+      Contact Us
+      </Text>
+      <Flex flexDirection="column" width={{ base:"100%", lg:"80%" }} fontSize="17px">
+        <Text> We’d love to connect with you. Here are some ways you can get in touch.</Text>
+        <List styleType="disc">
+          <ListItem><Link href="https://docs.google.com/forms/d/1l8QlNRfG2VTuoGI2sgeeZLHNfEJt701qlx7f7fy8ZUQ/edit" color="blue.400">Contact us about using our technology and services</Link></ListItem>
+          <ListItem><Link href="https://docs.google.com/forms/d/e/1FAIpQLSdBg0nVcMyhuPyNUGpDydzj26CmuCjcOuR_EZ3H99XB6xyHHw/viewform" color="blue.400">Join our community as a volunteer</Link></ListItem>
+          <ListItem><Link href="https://github.com/Path-Check" color="blue.400">Visit our GitHub repo</Link></ListItem>
+          <ListItem><Link href="mailto:media@pathcheck.org" color="blue.400">Submit a media inquiry</Link></ListItem>
+          <ListItem><Link href="https://pathcheck.org/events" color="blue.400">Attend a webinar or online symposium</Link></ListItem>
+        </List>
+        <Text fontWeight="bold">Follow Us on Social Media</Text>
+      </Flex>
+    </Flex>
+
+    <Flex py="20px"> 
+      <Text width={{ base:"100%", lg:"20%" }} fontSize="25px">
+      Supporters
+      </Text>
+      <Text width={{ base:"100%", lg:"80%" }} fontSize="17px">
+      Many companies are donating goods and services to PathCheck Foundation, which makes our work possible. We’re grateful for this support. Some of our supporters include:
+      </Text>
+      <Flex>
+        <Image src="../images/cisco-logo.png"/>
+      </Flex>
+    </Flex>
+
+    <Flex py="20px"> 
+      <Text width={{ base:"100%", lg:"20%" }} fontSize="25px">
+      In the news
+      </Text>
+      <Flex width={{ base:"100%", lg:"80%" }} wrap="wrap">
+        <Box width={{ base:"100%", md:"50%" }}>
+          <Text fontSize="14px">06.03.2020 • The New York Times</Text>
+          <Link href="https://www.nytimes.com/2020/06/03/health/coronavirus-contact-tracing-apps.html" fontSize="19px" fontWeight="bold">Contact Tracing With Your Phone: It’s Easier but There Are Tradeoffs</Link>
+        </Box>
+        <Box width={{ base:"100%", md:"50%" }}>
+          <Text fontSize="14px">06.03.2020 • The New York Times</Text>
+          <Link href="https://www.nytimes.com/2020/06/03/health/coronavirus-contact-tracing-apps.html" fontSize="19px" fontWeight="bold">Contact Tracing With Your Phone: It’s Easier but There Are Tradeoffs</Link>
+        </Box>
+      </Flex>
+    </Flex>
+
   </DefaultLayout>
 );
 
